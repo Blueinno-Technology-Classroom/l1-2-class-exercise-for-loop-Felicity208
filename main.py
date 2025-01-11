0de00bfb67d2aa64fdd70cda8e933c9cfce044bb
Felicity <felicity.curreem@gmail.com>
2025-01-11 12:03:21 +0800

    L1 - part 2

--- main.py
+++ main.py
@@ -40,4 +40,43 @@ Q6:
 
 # TODO: Write your code here
 
+print('Q6:')
+for i in range (5):
+    print('*' * 5)
+print('#' * 30)
+
+##################################################
+'''
+Q7:
+'''
+
+# TODO: Write your code here
+
+print('Q7:')
+for i in range (1, 6, 1):
+    print("*" * i)
+print('#' * 30)
+
+##################################################
+'''
+Q8:
+'''
+
+# TODO: Write your code here
+
+print('Q8:')
+for i in range (6, 0, -1):
+    print("*" * i)
+print('#' * 30)
+##################################################
+'''
+Q9:
+'''
+
+# TODO: Write your code here
+
+print('Q9:')
+for i in range (1, 6, 1):
+    print(f'{i}' * i)
+print('#' * 30)
 ##################################################
